bb75da0530cc60481a2ec7beccf598a08af17aed
Kitty <kathryn.harris@ucdenver.edu>
2021-12-20 19:30:31 -0700

    Fix ordering so that python script can run while file exists. Don't know why the file stopped existing but this seems to work.

--- tests/isempty.py
+++ tests/isempty.py
@@ -1,4 +1,4 @@
 import uproot
 file = uproot.open('output.root') #Change this to change tested file.
-if len(file['cascades'].keys()) == 0:
-	raise ValueError('No keys found under `cascades` - file appears to be empty!')
+if len(file['cascade'].keys()) == 0:
+	raise ValueError('No keys found under `cascade` - file appears to be empty!')
--- tests/test_realizeCascades.sh
+++ tests/test_realizeCascades.sh
@@ -4,8 +4,8 @@
 #alias md5sum='md5'
 
 realizeCascades -n 100000 -o "output.root?reproducible=fixedname" -d 1 ../levelfiles/Si28_ngam_all_cascades_rfmt_sorted.txt 
-#> /dev/null
+# Check that file is uncorrupted and has data
+python3 ./isempty.py
+# Check md5sum
 md5sum output.root
 md5sum output.root | awk '{if ($1=="2029e8b402b89d3a05d745e48f546370"){system("rm output.root"); print"Checksum passed!"} else {system("rm output.root"); exit 1}}'
-# Check that file is uncorrupted and has data
-python ./isempty.py
