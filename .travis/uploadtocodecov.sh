8583578e3c473dc845266ac62fc52737b4ff0319
Anthony Villano <villaa@gmail.com>
2022-02-06 14:06:56 -0700

    [ANV] tweak coverage info

--- .travis/uploadtocodecov.sh
+++ .travis/uploadtocodecov.sh
@@ -7,7 +7,7 @@ genhtml nrCascadeSim_coverage.info
 #Download the main uploader script
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 #Check the uploader script
-curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --impor$
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
 curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
 curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
