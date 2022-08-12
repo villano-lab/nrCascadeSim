d236d8d3850743c937c7a2dade697f2d6d23b793
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-08-12 17:25:52 -0600

    Try creating a regular env var from matrix.os

--- tests/test_realizeCascades.sh
+++ tests/test_realizeCascades.sh
@@ -20,15 +20,16 @@ fi
         #export ${{ matrix.os }}=$(echo $TRAVIS_OSX_IMAGE | sed 's/\./-/g' | sed 's/\//-/g')
         #echo finished sed
 #fi
-echo OS: ${{ matrix.os }}
+setenv MATRIX_OS = ${{ matrix.os }}
+echo OS: $MATRIX_OS
 export ROOT_VERSION=$(echo $ROOTDIST | cut -c 7-13 | sed 's/\./-/g')
 echo ROOT_VERSION: $ROOT_VERSION
 for input in Si28_ngam_all_cascades_rfmt_sorted 20ne_ngam allge_ngam_WFast allge_ngam_WSlow 40ar_ngam v3_natSi; do
-        DYLD_LIBRARY_PATH="${ROOT_LIB}" realizeCascades -sn 100000 -o "${input}_${ROOT_VERSION}_${{ matrix.os }}.root?reproducible=fixedname" -d 1 ../levelfiles/$input.txt
+        DYLD_LIBRARY_PATH="${ROOT_LIB}" realizeCascades -sn 100000 -o "${input}_${ROOT_VERSION}_$MATRIX_OS.root?reproducible=fixedname" -d 1 ../levelfiles/$input.txt
         if [[ "$OSTYPE" == "darwin"* ]]; then
-                md5 -r "${input}_${ROOT_VERSION}_${{ matrix.os }}}.root"
+                md5 -r "${input}_${ROOT_VERSION}_$MATRIX_OS.root"
         else
-                md5sum "${input}_${ROOT_VERSION}_${{ matrix.os }}}.root"
+                md5sum "${input}_${ROOT_VERSION}_$MATRIX_OS.root"
         fi
 done
 
