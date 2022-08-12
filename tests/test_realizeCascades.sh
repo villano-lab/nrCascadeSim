265347d16911d0de45f29ff42c460a3394580ae4
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-08-12 16:34:26 -0600

    Catch some more md5sum calls.

--- tests/test_realizeCascades.sh
+++ tests/test_realizeCascades.sh
@@ -14,8 +14,8 @@ fi
 # Check md5sum for all tested inputfiles.
 
 #Set up to generate filenames. Need to remove `.` and `/` special characters by changing them to `-`.
-echo TRAVIS_OS_NAME: $TRAVIS_OS_NAME
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+echo TRAVIS_OS_NAME: ${{ matrix.os }}
+if [ "${{ matrix.os }}" == "osx" ]; then
         #echo went into if
         export TRAVIS_DIST=$(echo $TRAVIS_OSX_IMAGE | sed 's/\./-/g' | sed 's/\//-/g')
         #echo finished sed
@@ -25,6 +25,11 @@ export ROOT_VERSION=$(echo $ROOTDIST | cut -c 7-13 | sed 's/\./-/g')
 echo ROOT_VERSION: $ROOT_VERSION
 for input in Si28_ngam_all_cascades_rfmt_sorted 20ne_ngam allge_ngam_WFast allge_ngam_WSlow 40ar_ngam v3_natSi; do
         DYLD_LIBRARY_PATH="${ROOT_LIB}" realizeCascades -sn 100000 -o "${input}_${ROOT_VERSION}_${TRAVIS_DIST}.root?reproducible=fixedname" -d 1 ../levelfiles/$input.txt
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+                md5 -r "${input}_${ROOT_VERSION}_${TRAVIS_DIST}.root"
+        else
+                md5sum "${input}_${ROOT_VERSION}_${TRAVIS_DIST}.root"
+        fi
 	md5sum "${input}_${ROOT_VERSION}_${TRAVIS_DIST}.root"
 done
 
