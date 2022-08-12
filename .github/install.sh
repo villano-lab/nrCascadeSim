28a42e2fde78089ab96a252f73f96e2fbb1f17ac
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-08-12 16:01:08 -0600

    Try using alias instead of installing md5sha1sum Seems to be functioning differently.

--- .github/install.sh
+++ .github/install.sh
@@ -6,7 +6,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
     clang --version
-    brew install md5sha1sum
+    alias md5sum="md5 -r"
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
     bash ~/miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
