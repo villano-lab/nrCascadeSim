55d843598887fd66b70980a35a8f4704d085d82c
Kitt <kathryn.harris@ucdenver.edu>
2022-08-17 13:43:34 -0600

    Add some troubleshooting statements
    regarding passing of matrix values.

--- tests/test_realizeCascades.sh
+++ tests/test_realizeCascades.sh
@@ -21,9 +21,9 @@ fi
         #echo finished sed
 #fi
 setenv MATRIX_OS = ${{ matrix.os }}
-echo OS: $MATRIX_OS
+echo OS: $MATRIX_OS ( ${{ matrix.os }})
 export ROOT_VERSION=$(echo $ROOTDIST | cut -c 7-13 | sed 's/\./-/g')
-echo ROOT_VERSION: $ROOT_VERSION
+echo ROOT_VERSION: $ROOT_VERSION ( ${{ matrix.rootver }})
 for input in Si28_ngam_all_cascades_rfmt_sorted 20ne_ngam allge_ngam_WFast allge_ngam_WSlow 40ar_ngam v3_natSi; do
         DYLD_LIBRARY_PATH="${ROOT_LIB}" realizeCascades -sn 100000 -o "${input}_${ROOT_VERSION}_$MATRIX_OS.root?reproducible=fixedname" -d 1 ../levelfiles/$input.txt
         md5sum "${input}_${ROOT_VERSION}_$MATRIX_OS.root"
