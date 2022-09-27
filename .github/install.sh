a29af50adfec4fbd330a86365e3c222e8a84efae
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-09-27 14:11:44 -0600

    Install cmake through snap to get newer version

--- .github/install.sh
+++ .github/install.sh
@@ -47,7 +47,8 @@ else
     #get apt-get and conda
     sudo apt-get update -y || true
     sleep 10
-    sudo apt-get install -y make cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev openssl libssl-dev curl
+    sudo apt-get install -y make g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev openssl libssl-dev curl
+    snap install cmake --version
     sleep 10
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -p $HOME/miniconda
