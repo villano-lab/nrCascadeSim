9b98dd0af971ddf5a820fba4634cad03a6b9151c
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-09-27 12:15:02 -0600

    add -y arg to install commands and split env vars

--- .github/install.sh
+++ .github/install.sh
@@ -45,9 +45,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
     # Install some custom requirements on Linux
     #get apt-get and conda
-    sudo apt-get update || true
+    sudo apt-get update -y || true
     sleep 10
-    sudo apt-get install make cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev openssl libssl-dev
+    sudo apt-get install -y make cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev openssl libssl-dev
     sleep 10
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -p $HOME/miniconda
